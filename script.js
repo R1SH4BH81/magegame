5d54ae429dba3a0f4128aacc733d027bfa09c021
RISHABH MISHRA <92362688+R1SH4BH81@users.noreply.github.com>
2023-08-05 00:01:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4792,7 +4792,7 @@ var init_map = function(map) {
     map.data.solid_tiles = [1,2,3,4];
     map.enemy_tile = 9;
     var img = new Image();
-    img.src = "https://i.imgur.com/epSeAdJ.jpg";
+    
     map.background_img = img;
 };
 
@@ -7300,7 +7300,7 @@ var init_map = function(map) {
     map.data.solid_tiles = [1,2,3,4];
     map.enemy_tile = 9;
     var img = new Image();
-    img.src = "https://i.imgur.com/epSeAdJ.jpg";
+    
     map.background_img = img;
 };
 
